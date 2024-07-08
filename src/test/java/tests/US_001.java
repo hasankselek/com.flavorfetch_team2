46228588ff21e00fae23565995749a0d56a17617
Hasan Küçükselek <haskselek@hotmail.com>
2024-07-09 01:24:44 +0300

    hasan pom.xml uptade

--- src/test/java/tests/US_001.java
+++ src/test/java/tests/US_001.java
@@ -1,11 +1,21 @@
 package tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
+import utilities.ConfigReader;
+import utilities.Driver;
 
 public class US_001 {
 
     @Test
     public void TC_0101(){
+
+        Driver.getDriver().get(ConfigReader.getProperty("customer_Url"));
+
+        String expectedUrl = "https://qa.flavorfetch.com/" ;
+        String actualUrl = Driver.getDriver().getCurrentUrl();
+
+        Assert.assertEquals(actualUrl,expectedUrl);
 
     }
 
