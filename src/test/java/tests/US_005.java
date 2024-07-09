a15486dac0e96b12d655b7a4f0be7ed688a02a01
gulpembe <gulpembe.gemicioglu@hotmail.com>
2024-07-09 23:04:14 +0300

    gulpembe_09.07

--- src/test/java/tests/US_005.java
+++ src/test/java/tests/US_005.java
@@ -1,4 +1,135 @@
 package tests;
 
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.CustomerPage;
+import utilities.ConfigReader;
+import utilities.Driver;
+
 public class US_005 {
+
+    //As a user, I want to be able to login with my email and password information that I registered on the site.
+
+    CustomerPage customerPage = new CustomerPage();
+
+    @Test
+    public void TC_0501 (){
+
+
+        //Kullanici browseri acar
+        //Url'I girer
+        Driver.getDriver().get(ConfigReader.getProperty("customer_Url"));
+        //Anasayfa header bolumundeki 'Sign in' butonuna tiklar
+        customerPage.signInButton.click();
+        //Email/telefon numarasi girebilmek icin bir box oldugunu dogrular
+        Assert.assertTrue(customerPage.emailBox.isDisplayed());
+        //Password girebilmek icin bir box oldugunu dogrular
+        Assert.assertTrue(customerPage.passwordBox.isDisplayed());
+        //Password girilecek alanin zorunlu oldugunu dogrular
+        Assert.assertTrue(customerPage.passwordBox.getAccessibleName().contains("*"));
+        //Password'u gizleyip/acmak icin goz ikonu oldugunu dogrular
+        Assert.assertTrue(customerPage.passworrdHideShowEye.isDisplayed());
+        //Remember me' secenegi icin bir checkbox oldugunu dogrular
+        Assert.assertTrue(customerPage.rememberMeCheckBox.isDisplayed());
+        //Forgot password?' secenegi icin bir link baglantisi oldugunu dogrular
+        Assert.assertTrue(customerPage.forgotPasswordLink.isDisplayed());
+        //Yeni kayitlar icin bir 'Sign Up' link baglantisi oldugunu dogrular
+        Assert.assertTrue(customerPage.signUpButton.isDisplayed());
+        //Sayfayi kapatir
+        Driver.getDriver().quit();
+
+    } // Test of "Email","Password","Forgot password?", "Remember me"  and "Sign Up" fields
+
+    @Test
+    public void TC_0502 (){
+        //Kullanici browseri acar
+        //Url'I girer
+        Driver.getDriver().get(ConfigReader.getProperty("customer_Url"));
+        //Anasayfa header bolumundeki 'Sign in' butonuna tiklar
+        customerPage.signInButton.click();
+        //Email/telefon numarasi alanina dogru emaili girer
+        customerPage.emailBox.sendKeys(ConfigReader.getProperty("customeruser_gülpembe"));
+        //Password alanina dogru passwordu girer
+        customerPage.passwordBox.sendKeys(ConfigReader.getProperty("customerpassword_gülpembe"));
+        //Sign in butonuna tiklar ve basarili giris yapabildigini dogrular
+        customerPage.loginSigninButton.click();
+        Assert.assertTrue(customerPage.notificicationsButton.isDisplayed());
+        //Hesaptan cikis yapar
+        customerPage.profileDropdownMenu.click();
+        customerPage.logoutButton.click();
+        //Sayfayi kapatir
+        Driver.getDriver().quit();
+
+
+    } // Test of successful logging in with the correct email and correct password
+
+    @Test
+    public void TC_0503 (){
+
+        //Kullanici browseri acar
+        //Url'I girer
+        Driver.getDriver().get(ConfigReader.getProperty("customer_Url"));
+        //Anasayfa header bolumundeki 'Sign in' butonuna tiklar
+        customerPage.signInButton.click();
+        //Email/telefon numarasi alanina dogru telefon numarasini girer
+        customerPage.emailBox.sendKeys(ConfigReader.getProperty("phoneNumber_gulpembe"));
+        //Password alanina dogru passwordu girer
+        customerPage.passwordBox.sendKeys(ConfigReader.getProperty("customerpassword_gülpembe"));
+        //Sign in butonuna tiklar ve basarili giris yapabildigini dogrular
+        customerPage.loginSigninButton.click();
+        Assert.assertTrue(customerPage.notificicationsButton.isDisplayed());
+        //Hesaptan cikis yapar
+        customerPage.profileDropdownMenu.click();
+        customerPage.logoutButton.click();
+        //Sayfayi kapatir
+        Driver.getDriver().quit();
+
+
+    } // Test of successful logging in with the correct phone number and correct password
+
+    @Test
+    public void TC_0504 (){
+
+        //Kullanici browseri acar
+        //Url'I girer
+        Driver.getDriver().get(ConfigReader.getProperty("customer_Url"));
+        //Anasayfa header bolumundeki 'Sign in' butonuna tiklar
+        customerPage.signInButton.click();
+        //Email/telefon numarasi alanina hatali email girer
+        customerPage.emailBox.sendKeys(ConfigReader.getProperty("incorrectCustomeruser_gülpembe"));
+        //Password alanina dogru passwordu girer
+        customerPage.passwordBox.sendKeys(ConfigReader.getProperty("customerpassword_gülpembe"));
+        //Sign in butonuna tiklar ve basarili giris yapilamadigini dogrular
+        customerPage.loginSigninButton.click();
+        Assert.assertTrue(customerPage.loginAlert.isDisplayed());
+        //Sayfayi kapatir
+        Driver.getDriver().quit();
+
+    } // Test of unsuccessful logging in with the incorrect email and correct password
+
+    @Test
+    public void TC_0505 (){
+
+        //Kullanici browseri acar
+        //Url'I girer
+        Driver.getDriver().get(ConfigReader.getProperty("customer_Url"));
+        //Anasayfa header bolumundeki 'Sign in' butonuna tiklar
+        customerPage.signInButton.click();
+        //Email/telefon numarasi alanina dogru emaili girer
+        customerPage.emailBox.sendKeys(ConfigReader.getProperty("customeruser_gülpembe"));
+        //Password alanina hatali password girer
+        customerPage.passwordBox.sendKeys(ConfigReader.getProperty("incorrectCustomerpassword_gülpembe"));
+        //Sign in butonuna tiklar ve basarili giris yapilamadigini dogrular
+        customerPage.loginSigninButton.click();
+        Assert.assertTrue(customerPage.loginAlert.isDisplayed());
+        //Sayfayi kapatir
+        Driver.getDriver().quit();
+
+
+    } // Test of unsuccessful logging in with the correct email and incorrect password
+
+
+
 }
