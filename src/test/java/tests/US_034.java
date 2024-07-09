b6210c1aa272769927f040ba80950a01a5ac21d6
SelahattinFurkan <154260536+SelahattinFurkan@users.noreply.github.com>
2024-07-09 12:18:47 +0300

    selahattin1

--- src/test/java/tests/US_034.java
+++ src/test/java/tests/US_034.java
@@ -2,12 +2,14 @@ package tests;
 
 import org.testng.annotations.Test;
 import utilities.Driver;
+import utilities.ReusableMethods;
 
 public class US_034 {
 
     @Test
     public void TC_3401(){
         Driver.getDriver().get("customer_Url");
+        ReusableMethods.wait2(2);
         Driver.quitDriver();
     }
 }
