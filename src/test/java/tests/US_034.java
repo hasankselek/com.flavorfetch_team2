65dc2adb9ff4e77bed080f2779023494e6530da2
SelahattinFurkan <154260536+SelahattinFurkan@users.noreply.github.com>
2024-07-09 15:33:26 +0300

    selahattin15.30

--- src/test/java/tests/US_034.java
+++ src/test/java/tests/US_034.java
@@ -1,6 +1,8 @@
 package tests;
 
 import org.testng.annotations.Test;
+import pages.MerchantPage;
+import utilities.ConfigReader;
 import utilities.Driver;
 import utilities.ReusableMethods;
 
@@ -8,7 +10,13 @@ public class US_034 {
 
     @Test
     public void TC_3401(){
-        Driver.getDriver().get("customer_Url");
+        Driver.getDriver().get(ConfigReader.getProperty("merchant_Url"));
+        MerchantPage merchantPage = new MerchantPage();
+        merchantPage.usernametextbox.sendKeys(ConfigReader.getProperty("merchant_username"));
+        merchantPage.passwordtextbox.sendKeys(ConfigReader.getProperty("merchant_password"));
+        merchantPage.singinButton.click();
+        merchantPage.ordertypeLinki.click();
+        merchantPage.deliveryLinki.click();
         ReusableMethods.wait2(2);
         Driver.quitDriver();
     }
