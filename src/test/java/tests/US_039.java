3c1cbf7302c9eaef09b19e5eba4c4989b1072052
Hasan Küçükselek <haskselek@hotmail.com>
2024-07-13 17:51:59 +0300

    Hasan/13.07.2024

---
+++ src/test/java/tests/US_039.java
@@ -0,0 +1,14 @@
+package tests;
+
+import org.testng.annotations.Test;
+import pages.AdminPage;
+
+public class US_039 {
+
+    AdminPage adminPage = new AdminPage();
+
+    @Test
+    public void TC_3901(){
+        
+    }
+}
