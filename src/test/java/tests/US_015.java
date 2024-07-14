8565e93b46f62d15d5423813cb19dade4c81c2d7
MehmetÖzdemir <mehmet.ozzdemir@hotmail.com>
2024-07-14 12:27:10 +0300

    14.07.24 Mehmet

--- src/test/java/tests/US_015.java
+++ src/test/java/tests/US_015.java
@@ -1,4 +1,28 @@
 package tests;
 
+import org.testng.annotations.Test;
+
 public class US_015 {
+
+    @Test
+    public void TC_1501(){
+
+    }
+    @Test
+    public void TC_1502(){
+
+    }
+    @Test
+    public void TC_1503(){
+
+    }
+    @Test
+    public void TC_1504(){
+
+    }
+    @Test
+    public void TC_1505(){
+
+    }
+
 }
