a4bbc5e1b7899dce2fe81fc2bbfb1bb61b4a986b
Cpik <cepik.kbrnr@gmail.com>
2024-07-15 22:17:13 +0300

    kubra/15.07.2024

--- src/test/java/pages/MerchantPage.java
+++ src/test/java/pages/MerchantPage.java
@@ -294,10 +294,8 @@ public class MerchantPage {
     public  WebElement dasboardNewOrdersIcon;//merhant Page
 
     @FindBy(xpath = "(//p[@class='m-0'])[7]")
-    public List<WebElement> orderId1;//merchant_New order page
+    public List <WebElement> orderId1;//merchant_New order page
   
-    @FindBy (xpath ="(//p[@class='m-0'])[11]" )
-    public List<WebElement> orderId2;
 
     @FindBy(xpath = " (//p[@class='m-0'])[5]")
     public  WebElement customerName;//merchant_New order page
@@ -416,7 +414,7 @@ public class MerchantPage {
     @FindBy(xpath = "//span[text()='Reject order']")
     public  WebElement cancelRejectOrderButton;;//merchant_New order page
 
-    @FindBy(xpath = "//div[@class='notyf-announcer']")
+    @FindBy(xpath = "//div[text()='Order is cancelled']")
     public  WebElement cancelOrderMessage;;//merchant_New order page
 
     @FindBy(xpath = "//a[text()='Download PDF (A4)']")
@@ -632,10 +630,15 @@ public class MerchantPage {
     @FindBy(xpath = "//*[@class='col-md-5']")
     public WebElement orderView;
 
+    @FindBy(xpath = "//*[@class='row align-items-start']")
+    public List<WebElement>  newOrdersList;
 
 
 
 
 
-  
+
+
+
+
 }
