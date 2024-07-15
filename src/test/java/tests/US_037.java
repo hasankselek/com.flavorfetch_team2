c41f127193717e039a3f2f78cccdb6b93eceb78f
MehmetÖzdemir <mehmet.ozzdemir@hotmail.com>
2024-07-15 20:28:09 +0300

    15.07.24 Mehmet

--- src/test/java/tests/US_037.java
+++ src/test/java/tests/US_037.java
@@ -5,31 +5,27 @@ import org.testng.annotations.Test;
 public class US_037 {
 
     @Test
-    public void TC_3701(){
+    public void TC_3701() {
 
     }
+
     @Test
-    public void TC_3702(){
+    public void TC_3702() {
 
     }
+
     @Test
-    public void TC_3703(){
+    public void TC_3703() {
 
     }
+
     @Test
-    public void TC_3704(){
+    public void TC_3704() {
 
     }
-    @Test
-    public void TC_3705(){
 
-    }
     @Test
-    public void TC_3706(){
-
-    }
-    @Test
-    public void TC_3707(){
+    public void TC_3705() {
 
     }
 }
